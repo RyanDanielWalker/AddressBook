380075c38c934d3b7fc76d7ad34ba04f98b0148b
Ryan Walker <ryandanielwalker@gmail.com>
2021-04-05 11:58:52 -0700

    WIP: testing function for adding tasks
    Co-authored-by: JesseWhite <jesse.white6@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,24 @@ function AddressBook() {
   this.currentId = 0;
 }
 
-AddressBook.prototype.addContact = function(contact) {
+AddressBook.prototype.addContact = function (contact) {
   contact.id = this.assignId();
   this.contacts[contact.id] = contact;
 }
 
-AddressBook.prototype.assignId = function() {
+AddressBook.prototype.assignId = function () {
   this.currentId += 1;
   return this.currentId;
 }
 
-AddressBook.prototype.findContact = function(id) {
+AddressBook.prototype.findContact = function (id) {
   if (this.contacts[id] != undefined) {
     return this.contacts[id];
   }
   return false;
 }
 
-AddressBook.prototype.deleteContact = function(id) {
+AddressBook.prototype.deleteContact = function (id) {
   if (this.contacts[id] === undefined) {
     return false;
   }
@@ -36,15 +36,15 @@ function Contact(firstName, lastName, phoneNumber) {
   this.phoneNumber = phoneNumber;
 }
 
-Contact.prototype.fullName = function() {
+Contact.prototype.fullName = function () {
   return this.firstName + " " + this.lastName;
 }
 // Business Logic for Destinations
-function Destination(country, city, name, notes){
-this.country = country;
-this.city = city;
-this.name = name;
-this.notes = notes;
+function Destination(country, city, name, notes) {
+  this.country = country;
+  this.city = city;
+  this.name = name;
+  this.notes = notes;
 }
 
 // Business Logic for PlacesIHaveBeen
@@ -52,54 +52,71 @@ function PlacesIHaveBeen() {
   this.destinations = {}
   this.currentId = 0
 }
-PlacesIHaveBeen.prototype.assignId = function() {
+PlacesIHaveBeen.prototype.assignId = function () {
   this.currentId += 1
   return this.currentId
 }
-PlacesIHaveBeen.prototype.addDestination = function(destination) {
+PlacesIHaveBeen.prototype.addDestination = function (destination) {
   destination.id = this.assignId()
   this.destinations[destination.id] = destination
 }
-PlacesIHaveBeen.prototype.findDestination = function(id){
-    if (this.destinations[id] != undefined){
-        return this.destinations[id];
-    } else{
-        return false;
-    }
+PlacesIHaveBeen.prototype.findDestination = function (id) {
+  if (this.destinations[id] != undefined) {
+    return this.destinations[id];
+  } else {
+    return false;
+  }
 }
-PlacesIHaveBeen.prototype.deleteDestination = function(id){
-    if (this.destinations[id] === undefined){
-        return false;
-    } else {
-        delete this.destinations[id];
-        return true;
-    }
+PlacesIHaveBeen.prototype.deleteDestination = function (id) {
+  if (this.destinations[id] === undefined) {
+    return false;
+  } else {
+    delete this.destinations[id];
+    return true;
+  }
 }
 // Business Logic for TaskList
 function TaskList() {
   this.tasks = {}
   this.currentId = 0
 }
-TaskList.prototype.assignId = function() {
+TaskList.prototype.assignId = function () {
   this.currentId += 1
   return this.currentId
 }
 
-TaskList.prototype.addTask = function(task) {
+TaskList.prototype.addTask = function (task) {
   task.id = this.assignId()
   this.tasks[task.id] = task
 }
-TaskList.prototype.deleteTask = function(id) {
+TaskList.prototype.deleteTask = function (id) {
   if (this.tasks[id] === undefined) {
     return this.tasks[id]
   }
   delete this.tasks[id]
   return true
 }
-  
 
 // Business Logic for Tasks
 function Tasks() {
   this.task = task
 }
 
+let masterList = new TaskList()
+
+$(document).ready(function () {
+  $("#task").submit(function (event) {
+    event.preventDefault()
+
+    let newTask = $("#user-input").val()
+    taskToAdd = new Tasks(newTask)
+    masterList.addTask(taskToAdd)
+    console.log(masterList)
+  })
+})
+
+
+let tasks = ["task 1", "task 2"]
+tasks.forEach(function (task) {
+  masterList.addTask(task)
+})
